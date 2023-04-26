523db1b6593962f727b7484cc9cf721817da87c1
hicode-dev <kingdavies2021@gmail.com>
2023-04-26 01:21:19 -0700

    just added some commit

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,11 +2,12 @@
 import React from 'react'
 import {TbArrowsRightLeft,TbWallet,TbChartBar} from 'react-icons/tb'
 import {BsArrowRepeat} from 'react-icons/bs'
+import phone1 from '../../public/iPhones (1).png'
 
 const Box = () => {
     return (
        <div className='justify-center flex items-center py-20'>
-         <div className='text-white grid md:grid-cols-3 grid-cols-1  '>
+         <div className='text-white grid lg:grid-cols-3 grid-cols-1 overflow-hidden'>
           <div className='px-2'>
           <div className="box bg-[#010D50] my-4 m-auto flex-col  px-5 flex justify-center items-baseline   rounded-lg h-[295px] w-[399px]" >
             <button className='bg-blue-700 px-4 py-3 my-5 shadow-lg rounded-lg '>
@@ -26,9 +27,12 @@ const Box = () => {
           </div>
        
             <div className="box bg-[#0328EE] h-[622px] w-[400px]">
-          <div className="box  my-4 m-auto flex-col  px-5 flex justify-center items-baseline   rounded-lg h-[295px] w-[399px]" >
+          <div className="box relative    m-auto flex-col  px-5 flex pt-7 text-center    rounded-lg h-[295px] w-[399px]" >
           <h4 className='text-[18px] font-bold pb-3 uppercase'>iOS & Android App </h4>
             <p className='text-[16px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse tortor aene.</p>
+            <div className='absolute top-[11.3rem] flex justify-center items-center ' >
+                <img src={phone1} alt="" />
+            </div>
 
           </div>
             </div>
--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -5,7 +5,7 @@ const Companies = () => {
   return (
     <div className='flex justify-center items-center flex-col text-white capitalize'>
       <p className=' font-bold'>Finance flow has been featured on</p>
-<div className="flex justify-between gap-16 mt-7 items-center">
+<div className="grid lg:grid-cols-7 gap-9 py-6 md:grid-cols-4 grid-cols-3 ">
 <h3 className='font-bold'>commpany</h3>
 <h3 className='font-bold'>commpany</h3>
 <h3 className='font-bold'>commpany</h3>
